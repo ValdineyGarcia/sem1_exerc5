1146d456d11c5cffa6edcda476bb8315a268cb5b
Valdiney Daniel Garcia <valdineysc@gmail.com>
2023-02-15 22:19:43 -0300

    Primeiro commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿double lado1, lado2, lado3;
+Console.WriteLine("Análise de triângulos! Informe o lado 1:");
+lado1 = double.Parse(Console.ReadLine());
+Console.WriteLine(" Informe o lado 2:");
+lado2 = double.Parse(Console.ReadLine());
+Console.WriteLine(" Informe o lado 3:");
+lado3 = double.Parse(Console.ReadLine());
+if (lado1 + lado2 > lado3 && lado3 + lado2 > lado1 && lado3 + lado1 > lado2) {
+Console.WriteLine("É um triângulo!");
+        if (lado1 == lado3 && lado2 == lado1) {
+    Console.WriteLine("Triângulo Equilátero.");
+    }
+        else if (lado1 ==lado2 || lado2 == lado3 || lado1 == lado3){
+    Console.WriteLine("Triângulo Isósceles.");
+    }
+        else if (lado1 != lado2 && lado2 != lado3 && lado1 != lado3){
+        Console.WriteLine("Triângulo Escaleno.");
+    }
+    }
+else{
+    Console.WriteLine("As medidas não formam um triângulo.");
+}
